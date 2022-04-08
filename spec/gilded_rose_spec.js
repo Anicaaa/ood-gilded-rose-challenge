f75faedb9b0b4cdfc99827c53fdb28217e7691d8
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-08 18:00:08 +0100

    item quality no more than 50 passed

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -22,4 +22,11 @@ describe('Gilded Rose', function () {
     expect(items[0].sellIn).toEqual(1)
     expect(items[0].quality).toEqual(2)
   })
+
+  it('Item quality should NOT be more than 50', function () {
+    const gildedRose = new Shop([new Item('Aged Brie', 5, 43)])
+    const items = gildedRose.updateQuality()
+    expect(items[0].name).toEqual('Aged Brie')
+    expect(items[0].quality).toEqual(44)
+  })
 })
