6c0a1271820ae010c1424d11529cd79b1f9fc566
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-08 17:10:16 +0100

    should decrease quality each day passed

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,17 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+var { Shop, Item } = require('../src/gilded_rose.js')
+describe('Gilded Rose', function () {
+  // item has name, sellIn, quality
+  it('should have quality always greater or equal to zero', function () {
+    const gildedRose = new Shop([new Item('foo', 0, 0)])
+    const items = gildedRose.updateQuality()
+    expect(items[0].name).toEqual('foo')
+    expect(items[0].quality).toEqual(0)
+  })
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
-
-});
+  it('should decrease quality each day', function () {
+    const gildedRose = new Shop([new Item('foo', 5, 1)]) //Added an item that has 5 days to sell, an initial quality of 1.
+    const items = gildedRose.updateQuality() //Updating quality because 1 day has passed, some items will change with different rules.
+    expect(items[0].name).toEqual('foo')
+    expect(items[0].quality).toEqual(0)
+  })
+})
