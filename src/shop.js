7a4dd23a65724638b633246c5c9c701fb9045059
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-11 17:44:51 +0100

    Created backstage function

--- src/shop.js
+++ src/shop.js
@@ -14,11 +14,15 @@ class Shop {
     }
   }
 
+  updateBackstage(item) {}
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       const item = this.items[i]
       if (item.name === 'Aged Brie') {
         this.updateAgedBrie(item)
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        this.updateBackstage(item)
       } else {
         if (
           item.name != 'Aged Brie' &&
@@ -30,21 +34,6 @@ class Shop {
             }
           }
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-              if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                  item.quality = item.quality + 1
-                }
-              }
-              if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                  item.quality = item.quality + 1
-                }
-              }
-            }
-          }
         }
         if (item.name != 'Sulfuras, Hand of Ragnaros') {
           item.sellIn = item.sellIn - 1
@@ -52,17 +41,18 @@ class Shop {
         if (item.sellIn < 0) {
           if (item.name != 'Aged Brie') {
             if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+              // <---- I want this to my updateBackstage method
               if (item.quality > 0) {
                 if (item.name != 'Sulfuras, Hand of Ragnaros') {
                   item.quality = item.quality - 1
                 }
               }
             } else {
-              item.quality = item.quality - item.quality
+              item.quality = item.quality - item.quality // <---- I want this to my updateBackstage method
             }
           } else {
             if (item.quality < 50) {
-              item.quality = item.quality + 1
+              item.quality = item.quality + 1 // <---- I want this to my updateBackstage method
             }
           }
         }
